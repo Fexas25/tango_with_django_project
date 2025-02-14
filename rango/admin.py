de8b5e0ca35fd2f688ea642e7d1624fd114219c2
“Wanting <2961179L@student.gla.ac.uk>
2025-02-14 10:39:28 +0000

    chapter5test

--- rango/admin.py
+++ rango/admin.py
@@ -9,4 +9,5 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
 admin.site.register(UserProfile)
